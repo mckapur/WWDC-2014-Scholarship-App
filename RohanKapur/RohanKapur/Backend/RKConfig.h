a0146b4759166a6ce8655de0e750108b45a35710
MCKapur <me@rohankapur.com>
2014-04-15 22:35:25 +0800

    Removed config from .gitignore

---
+++ RohanKapur/RohanKapur/Backend/RKConfig.h
@@ -0,0 +1,16 @@
+//
+//  RKConfig.h
+//  Rohan Kapur
+//
+//  Created by Rohan Kapur on 13/4/14.
+//  Copyright (c) 2014 Rohan Kapur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static __unused NSString* const kWolframAlphaQueryAppID = @"Q5YXLW-J5X5XWVAX8";
+static __unused NSString* const kWitAccessToken = @"FLVASKR4JAH2QOVNXJ5VQTFEOHXW5ESR";
+
+@interface RKConfig : NSObject
+
+@end
